cd91983d10f2c04f770ed42da2077b0d935a3e26
ayoubmouaddine2004 <ayoubmouaddine817@gmail.com>
2026-08-20 19:22:59 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const client = {
+
+    name: "maison crochet",
+
+    category: "article crochet",
+
+    description: "hande made article white love.",
+
+    banner: "/client/maison crochet/images/baner1.png",
+
+    logo: "/client/maison crochet/images/logo1.png",
+
+    phone: "+212700202866",
+
+    whatsapp: "212717354208",
+
+    instagram: "https://www.instagram.com/maison__crochet__",
+
+    address: "Casablanca, Maroc",
+
+    maps: "https://www.google.com/maps/place/%D8%A8%D8%AD%D9%8A%D8%B1%D8%A9+%D9%88%D9%8A%D9%88%D8%A7%D9%86%E2%80%AD/@33.1338203,-5.3469015,17z/data=!3m1!4b1!4m6!3m5!1s0xda1f3a3907243b5:0x86deb93869c63856!8m2!3d33.1338203!4d-5.3443266!16s%2Fg%2F11tt1t6zkm?entry=ttu&g_ep=EgoyMDI2MDgxNy4wIKXMDSoASAFQAw%3D%3D",
+
+    services: [
+
+        {
+            name: "sac crochet",
+    
+        },
+
+        {
+            name: "panier crochet",
+        
+        },
+
+        {
+            name: "accessoires crochet",
+        
+        }
+
+    ]
+
+};
